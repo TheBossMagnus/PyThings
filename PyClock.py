7704d7c117204e8ddaa5d879e5091baa3e714d84
TheBossMagnus <tbmagnusgamer@gmail.com>
2022-11-08 17:20:31 +0100

    1

--- PyClock.py
+++ PyClock.py
@@ -1,22 +1,34 @@
-import tkinter  as tk
+from curses.textpad import Textbox
+import tkinter as tk
 from pick import pick
+import time
 import datetime
+import threading
+import playsound
+import keyboard
+import psutil
 
 # --- Mini-clock ---
 
+
 def widgetclock():
+    def stop():
+        for proc in psutil.process_iter():
+            if proc.name() == "python.exe":
+                proc.kill()
+    keyboard.add_hotkey("ctrl+shift+z", stop)
     def update_clock():
         # get current time as text
         current_time = datetime.datetime.now().strftime("%H:%M:%S")
 
-        # udpate text in Label
-        lab.config(text=current_time, font=('Segoe UI (TrueType)',12))
-        #lab['text'] = current_time
+        # update text in Label
+        lab.config(text=current_time, font=('Segoe UI (TrueType)', 12))
+        # lab['text'] = current_time
 
         # run itself again after 1000 ms
         win.after(1000, update_clock)
 
-    #https://www.daniweb.com/programming/software-development/threads/66181/center-a-tkinter-window
+    # https://www.daniweb.com/programming/software-development/threads/66181/center-a-tkinter-window
     def center_window(w=65, h=20):
         # get screen width and height
         hs = win.winfo_screenheight()
@@ -35,19 +47,102 @@ def widgetclock():
     update_clock()
     win.mainloop()
 
-def FullscreenCloclk():
+
+ # thanks to https://youtu.be/kfjdVgKE6xY for threading
+class Timer:
+    def stop():
+        for proc in psutil.process_iter():
+            if proc.name() == "python.exe":
+                proc.kill()
+    keyboard.add_hotkey("ctrl+shift+z", stop)
+    def __init__(self):
+        self.win = tk.Tk()
+        self.win.title("Timer")
+        self.win.geometry("390x220")
+
+        self.time_entry = tk.Entry(self.win) #Textbox
+        self.time_entry.place(x=133, y=5, height=25)
+
+        self.time_label = tk.Label(self.win, text="00:00:00", font = "Helvetica 70 bold") #time
+        self.time_label.place(x=5, y=30)
+
+        self.start_button = tk.Button(self.win, text="Start", font = "Helvetica 16", command=self.start_thread) #start
+        self.start_button.place(x=90, y=165, height=30, width=60)
+
+        self.stop_button = tk.Button(self.win, text="Stop", font = "Helvetica 16", command=self.stop) #stop
+        self.stop_button.place(x=240, y=165, height=30, width=60)
+
+        self.stop_loop = False
+
+        self.win.mainloop()
+
+    def start_thread(self):
+        t = threading.Thread(target=self.start)
+        t.start()
+
+    def start(self):
+        hours, minutes, seconds = 0, 0, 0
+        string_split = self.time_entry.get().split(":")
+        if len(string_split) == 3:
+            hours = int(string_split[0])
+            minutes = int(string_split[1])
+            seconds = int(string_split[2])
+        elif len(string_split) == 2:
+            minutes = int(string_split[0])
+            seconds = int(string_split[1])
+        elif len(string_split) == 1:
+            seconds = int(string_split[0])
+        else:
+            print("Invalid input")
+            return
+
+        full_seconds = hours * 3600 + minutes * 60 + seconds
+
+        while full_seconds > 0 and not self.stop_loop:
+            full_seconds -= 1
+
+            minutes, seconds = divmod(full_seconds, 60)
+            hours, minutes = divmod(minutes, 60)
+
+            self.time_label.config(text=f"{hours:02d}:{minutes:02d}:{seconds:02d}")
+            self.win.update()
+            time.sleep(1)
+
+        if not self.stop_loop:
+            self.time_label.config(text="00:00:00")
+            self.win.wm_attributes('-topmost', 'True')
+            playsound.playsound("C:/Windows/Media/Alarm01.wav")  # use / instead of \ https://stackoverflow.com/questions/37400974/error-unicode-error-unicodeescape-codec-cant-decode-bytes-in-position-2-3
+
+    def stop(self):
+        self.stop_loop = True
+        self.time_label.config(text="00:00:00")
+
+
+
+
+
+def fullscreencloclk():
+    def stop():
+        for proc in psutil.process_iter():
+            if proc.name() == "python.exe":
+                proc.kill()
+    keyboard.add_hotkey("ctrl+shift+z", stop)
+    def stop():
+        win.destroy()
+        exit()
+    keyboard.add_hotkey("ctrl+shift+z", stop)
     def update_clock():
         # get current time as text
         current_time = datetime.datetime.now().strftime("%H:%M:%S")
 
-        # udpate text in Label
-        lab.config(text=current_time, font=('Segoe UI (TrueType)',250, 'bold'))
-        #lab['text'] = current_time
+        # update text in Label
+        lab.config(text=current_time, font=('Segoe UI (TrueType)', 250, 'bold'))
+        # lab['text'] = current_time
 
         # run itself again after 1000 ms
         win.after(1000, update_clock)
 
-    #https://www.daniweb.com/programming/software-development/threads/66181/center-a-tkinter-window
+    # https://www.daniweb.com/programming/software-development/threads/66181/center-a-tkinter-window
     def center_window(w=65, h=20):
         # get screen width and height
         hs = win.winfo_screenheight()
@@ -57,9 +152,9 @@ def FullscreenCloclk():
         win.geometry('%dx%d+%d+%d' % (w, h, x, y))
 
     win = tk.Tk()
-    #center_window(65, 20)
-    #win.overrideredirect(True)
-    #win.wm_attributes('-topmost', 'True')
+    # center_window(65, 20)
+    # win.overrideredirect(True)
+    # win.wm_attributes('-topmost', 'True')
     win.wm_attributes("-fullscreen", True)
     lab = tk.Label(win, text="00:00:00")
     lab.pack()
@@ -67,26 +162,29 @@ def FullscreenCloclk():
     update_clock()
     win.mainloop()
 
-#FullscreenCloclk()
+def Currenttime():
+    #currentDateAndTime = datetime.now()
+    print("The current date and time is")
 
-def Menu():
+def menu():
     title = 'Pyclock by TheBossMagnus'
-    options = ['Widget clock','Fullscreen clock','Widget timer','Fullscreen timer','exit']
+    options = ['Widget clock', 'Fullscreen clock','Timer','Current time', 'exit']
     option, options = pick(options, title)
 
     if option == 'Widget clock':
         widgetclock()
-        Menu()
+        menu()
     if option == 'Fullscreen clock':
-        FullscreenCloclk()
-        Menu()
-    if option == 'Widget timer':
-        print('SOON')
-        Menu()
-    if option == 'Fullscreen timer':
-        print('SOON')
-        Menu()
+        fullscreencloclk()
+        menu()
+    if option == 'Timer':
+        Timer()
+        menu()
+    if option == 'Current time':
+        Currenttime()
+        menu()
     if option == 'exit':
         exit()
 
-Menu()
+
+menu()
---
+++ import keyboard.py
@@ -0,0 +1,72 @@
+import ntplib
+from time import ctime
+import calendar
+import datetime
+
+
+
+response = ntplib.NTPClient().request('pool.ntp.org')
+output= response.tx_time
+converted=ctime(output)
+
+weekday, month, day, time, year =converted.split()
+hours, minutes, seconds = time.split(':')
+
+if weekday == 'Mon':
+    weekday = 'Monday'
+elif weekday == 'Tue':
+    weekday = 'Tuesday'
+elif weekday == 'Wed':
+    weekday = 'Wednesday'
+elif weekday == 'Thu':
+    weekday = 'Thursday'
+elif weekday == 'Fri':
+    weekday = 'Friday'
+elif weekday == 'Sat':
+    weekday = 'Saturday'
+elif weekday == 'Sun':
+    weekday = 'Sunday'
+
+
+if month == 'Jan':
+    month = 'January'
+elif month == 'Feb':
+    month = 'February'
+elif month == 'Mar':
+    month = 'March'
+elif month == 'Apr':
+    month = 'April'
+elif month == 'May':
+    month = 'May'
+elif month == 'Jun':
+    month = 'June'
+elif month == 'Jul':
+    month = 'July'
+elif month == 'Aug':
+    month = 'August'
+elif month == 'Sep':
+    month = 'September'
+elif month == 'Oct':
+    month = 'October'
+elif month == 'Nov':
+    month = 'November'
+elif month == 'Dec':
+    month = 'December'
+
+print(f"today is {weekday} {day} {month} {year}" )
+print(f"the time is {time}\n")
+
+today = datetime.date.today()
+days_in_current_month = calendar.monthrange(today.year, today.month)[1]
+days_till_end_month = days_in_current_month - today.day
+print("There are", 365 - today.timetuple().tm_yday, "days till the end of the year")
+print("There are", 52 - today.isocalendar()[1], "weeks till the end of the year")
+print("There are", 12 - today.month, "months till the end of the year\n")
+
+print(f"there are {days_till_end_month} days till the end of the month")
+print(f"there are {days_till_end_month//7} weeks till the end of the month\n")
+
+print("there are", 7 - today.weekday(), "days till the end of the week")
+
+
+
